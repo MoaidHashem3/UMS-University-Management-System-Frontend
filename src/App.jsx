8d80e0e41c6e31e88f5b7ede3f4c26151a843875
Moaid Hashem <Gamestorrent2015@gmail.com>
2024-10-01 03:59:33 +0300

    Adding sign up and sign in pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline'; 
 import theme from './theme'; 
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp'
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline /> 
+      <CssBaseline />
+      {/* <SignUp/> */}
+    
     </ThemeProvider>
   );
 };
