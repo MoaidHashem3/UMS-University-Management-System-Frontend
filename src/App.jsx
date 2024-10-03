8080d1f124865b027b5e639b810975abd02835aa
Moaid Hashem <71054199+MoaidHashem3@users.noreply.github.com>
2024-10-04 02:29:35 +0300

    Update app

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import SignIn from './pages/SignIn/SignIn'
+import SignUp from './pages/SignUp/SignUp'
+// import Dashboard from './pages/Dashboard/Dashboard'
+// import Quiz from './components/Quiz/QuizBox';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import AppLayout from './AppLayout';
+import { RouterProvider } from 'react-router-dom';
+import Courses from './pages/Courses/Courses';
+
+
+const App = () => {
+    const routes = createBrowserRouter([
+        {
+          path: "/", 
+          element: <AppLayout />, 
+          children: [
+            { index: true, element: <Home />  },
+            { path: 'signup', element: <SignUp /> },
+            { path: 'login', element: <SignIn /> },
+            { path: 'home', element: <Home /> },
+            { path: 'courses', element: <Courses /> },
+          ]
+        }
+      ]);
+    
+      return (
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <RouterProvider router={routes} />
+        </ThemeProvider>
+      );
+};
+
+export default App;
