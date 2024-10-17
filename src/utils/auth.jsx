afe7509609a54a9c055f225216fe36574adafd67
Moaid Hashem <Gamestorrent2015@gmail.com>
2024-10-17 10:47:58 +0300

    -Fixing Forget Password error

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -14,7 +14,7 @@ export const handleLogin = async (credentials) => {
 
 export const handleForgotPassword = async (email) => {
     try {
-        const response = await axios.post(`${API_URL}/forgot-password`, { email });
+        const response = await axiosInstance.post(`/forgot-password`, { email });
         return response.data; 
     } catch (error) {
         throw error; 
